584ba38c063da19e5eb47ed1a1050462a69ffce1
ill_Nino <ill_Nino@bk.ru>
2025-03-24 14:05:43 +0300

    feat(test): add solution part3

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -59,7 +59,7 @@ function getPromiseResult(source) {
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
 function getFirstResolvedPromiseResult(promises) {
-  return Promise.race(promises);
+  return Promise.any(promises);
 }
 
 /**
@@ -112,8 +112,12 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.all(
+    promises.map((promise) =>
+      promise.then((result) => result).catch(() => null)
+    )
+  );
 }
 
 /**
